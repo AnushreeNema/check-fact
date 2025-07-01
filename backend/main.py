f11691ff45a4e88cdee6d329b9aeb7ca8687ea42
Anushree Nema <anushreenema624@gmail.com>
2025-07-01 17:30:17 +0530

    open-ai api

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import wikipedia
+from dotenv import load_dotenv
+import openai
+import os
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = FastAPI()
 app.add_middleware(
