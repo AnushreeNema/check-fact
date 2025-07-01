0e4a23b4651a0565fdfec79657e654e2909e92c2
Anushree Nema <anushreenema624@gmail.com>
2025-07-01 16:41:14 +0530

    validity of source

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 function checkClaim() {
-  const claim = document.getElementById("claimInput").value;
-  const resultBox = document.getElementById("result");
-
-  resultBox.innerHTML = "checking...";
-  resultBox.className = "checking";
-}
-fetch("http://localhost:8000/fact-check", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ claim }),
-});
+    const claim = document.getElementById("claimInput").value;
+    const resultBox = document.getElementById("result");
+  
+    resultBox.innerHTML = "Checking...";
+    resultBox.className = "checking";
+  
+    fetch("http://localhost:8000/fact-check", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ claim }),
+    })
+      .then(res => res.json())
+      .then(data => {
+        resultBox.className = "result-box";
+        resultBox.innerHTML = `
+          <div class="verdict"><strong>Conclusion:</strong> ${data.verdict}</div>
+          <div class="reason"><strong>Reason:</strong> ${data.reason}</div>
+          <div class="source"><strong>Source:</strong> ${data.source}</div>
+        `;
+      })
